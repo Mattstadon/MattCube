f1d64da2ebebff830dd0888e300d652ee914a446
unknown <matthew.adams365@gmail.com>
2024-02-15 16:34:40 -0700

    LAYZOR has animation and SPARKS are working. Deleted IMAGE that darkened board after activation. Worked on minor aesthetics and tuned some animations and SFX.

---
+++ Assets/Scripts/Return.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static System.TimeZoneInfo;
+
+public class Return : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+    [SerializeField] SFXManager sfxManager; // Reference to the SFXManager
+    [SerializeField] AudioClip buttonClickSound; // Sound effect to play on click
+    [SerializeField] float buttonClickVolume = 1f; // Volume for the button click sound
+
+    private Button button; // Reference to the UI Button component
+
+    private void Awake()
+    {
+        button = GetComponent<Button>(); // Get the Button component
+        if (button != null)
+        {
+            button.onClick.AddListener(HandleButtonClick); // Subscribe to the onClick event
+        }
+    }
+
+    private void HandleButtonClick()
+    {
+        if (sfxManager != null && buttonClickSound != null)
+        {
+            sfxManager.PlaySFX(buttonClickSound, buttonClickVolume); // Play the sound effect
+        }
+    }
+    public void StartUp()
+    {
+        // Replace "MainGameScene" with the actual name of your main game scene
+        SceneManager.LoadScene("Boot Up");
+    }
+
+}
